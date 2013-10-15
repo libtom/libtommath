e2a0c20aacb457f1bcedb98215dee061687299de
czurnieden <czurnieden@gmx.de>
2013-10-16 00:37:36 +0200

    Added names of functions related to FFT multiplication
    FFT for 28 bit mp_digit's only for now

--- tommath.h
+++ tommath.h
@@ -193,7 +193,7 @@ typedef int ltm_prime_callback(unsigned char *dst, int len, void *dat);
 #define SIGN(m)    ((m)->sign)
 
 /* error code to char* string */
-char *mp_error_to_string(int code);
+const char *mp_error_to_string(int code);
 
 /* ---> init and deinit bignum functions <--- */
 /* init a bignum */
