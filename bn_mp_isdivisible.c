ccc36d72e2555ac75bed8f1b19051e7b16d66f1c
czurnieden <czurnieden@gmx.de>
2013-10-19 17:23:04 +0200

    Function to test if big nmber is divisible by another big number

---
+++ bn_mp_isdivisible.c
@@ -0,0 +1,17 @@
+#include <tommath.h>
+#ifdef BN_MP_ISDIVISIBLE_C
+int mp_isdivisible(mp_int * a, mp_int * d)
+{
+    int err;
+    mp_int res;
+    mp_init(&res);
+
+    if ((err = mp_mod(a, d, &res)) != MP_OKAY) {
+        return err;
+    }
+    if (mp_iszero(&res)) {
+        return MP_YES;
+    }
+    return MP_NO;
+}
+#endif
