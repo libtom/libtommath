46d685fc2582fb5e49d55c3551f7b01481dec0c5
czurnieden <czurnieden@gmx.de>
2013-10-19 17:34:12 +0200

    Returns zero-based index of highest bit.

---
+++ bn_mp_highbit.c
@@ -0,0 +1,9 @@
+#include <tommath.h>
+#ifdef BN_MP_HIGHBIT_C
+/* for convenience */
+int mp_highbit(mp_int * a)
+{
+    /* counting from zero such that highbit == lowbit for x = 2^n ; n>=0 */
+    return mp_count_bits(a) - 1;
+}
+#endif
