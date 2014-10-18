d71719853aafad80ed8570437910b767126f3369
Steffen Jaeckel <s@jaeckel.eu>
2014-10-18 18:56:56 +0200

    update after removing balance multiplication

--- tommath_class.h
+++ tommath_class.h
@@ -212,13 +212,6 @@
    #define BN_MP_CLEAR_C
 #endif
 
-#if defined(BN_MP_BALANCE_MUL_C)
-   #define BN_MP_GROW_C
-   #define BN_MP_INIT_SIZE_C
-   #define BN_MP_MUL_C
-   #define BN_MP_CLAMP_C
-#endif
-
 #if defined(BN_MP_CLAMP_C)
 #endif
 
@@ -587,7 +580,6 @@
    #define BN_FAST_S_MP_MUL_DIGS_C
    #define BN_S_MP_MUL_C
    #define BN_S_MP_MUL_DIGS_C
-   #define BN_MP_BALANCE_MUL_C
 #endif
 
 #if defined(BN_MP_MUL_2_C)
