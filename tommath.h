720ac3ef6dc1a57e40c132ef4f8728497bd2e37d
Francois Perrad <francois.perrad@gadz.org>
2015-10-17 18:27:56 +0200

    add parentheses for explicit operator association

--- tommath.h
+++ tommath.h
@@ -196,7 +196,7 @@ extern int KARATSUBA_MUL_CUTOFF,
 #endif
 
 /* size of comba arrays, should be at least 2 * 2**(BITS_PER_WORD - BITS_PER_DIGIT*2) */
-#define MP_WARRAY               (1 << ((sizeof(mp_word) * CHAR_BIT) - (2 * DIGIT_BIT) + 1))
+#define MP_WARRAY               (1 << (((sizeof(mp_word) * CHAR_BIT) - (2 * DIGIT_BIT)) + 1))
 
 /* the infamous mp_int structure */
 typedef struct  {
