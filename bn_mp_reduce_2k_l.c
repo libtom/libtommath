42970eb4275262764993caf7bd17e3594616754a
Samantha McVey <samantham@posteo.net>
2018-04-11 13:46:35 -0700

    Fix build on Solaris by using LBL_ERR instead of ERR
    libtommath won't build on Solaris 11.3 with a goto statement named
    `ERR`. Use LBL_ERR instead so it doesn't conflict.

--- bn_mp_reduce_2k_l.c
+++ bn_mp_reduce_2k_l.c
@@ -30,27 +30,27 @@ int mp_reduce_2k_l(mp_int *a, const mp_int *n, const mp_int *d)
 top:
    /* q = a/2**p, a = a mod 2**p */
    if ((res = mp_div_2d(a, p, &q, a)) != MP_OKAY) {
-      goto ERR;
+      goto LBL_ERR;
    }
 
    /* q = q * d */
    if ((res = mp_mul(&q, d, &q)) != MP_OKAY) {
-      goto ERR;
+      goto LBL_ERR;
    }
 
    /* a = a + q */
    if ((res = s_mp_add(a, &q, a)) != MP_OKAY) {
-      goto ERR;
+      goto LBL_ERR;
    }
 
    if (mp_cmp_mag(a, n) != MP_LT) {
       if ((res = s_mp_sub(a, n, a)) != MP_OKAY) {
-         goto ERR;
+         goto LBL_ERR;
       }
       goto top;
    }
 
-ERR:
+LBL_ERR:
    mp_clear(&q);
    return res;
 }
