4de6ea96167611dd7514c66c5446673da79e489e
Steffen Jaeckel <s@jaeckel.eu>
2018-12-02 21:18:10 +0100

    fixup updatemakes.sh
    [skip ci]

--- updatemakes.sh
+++ updatemakes.sh
@@ -3,7 +3,7 @@
 ./helper.pl --update-makefiles || exit 1
 
 makefiles=(makefile makefile.shared makefile_include.mk makefile.msvc makefile.unix makefile.mingw)
-vcproj=(libtomcrypt_VS2008.vcproj)
+vcproj=(libtommath_VS2008.vcproj)
 
 if [ $# -eq 1 ] && [ "$1" == "-c" ]; then
   git add ${makefiles[@]} ${vcproj[@]} && git commit -m 'Update makefiles'
