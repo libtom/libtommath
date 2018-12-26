29987bca9ac09c6482a8caa638e5f1a99d5a6460
Francois Perrad <francois.perrad@gadz.org>
2018-12-26 08:09:57 +0100

    fix cast

--- bn_mp_prime_is_prime.c
+++ bn_mp_prime_is_prime.c
@@ -307,7 +307,7 @@ int mp_prime_is_prime(const mp_int *a, int t, int *result)
          }
 #endif
          /* Ceil, because small numbers have a right to live, too, */
-         len = (int)((fips_rand + DIGIT_BIT) / DIGIT_BIT);
+         len = (((int)fips_rand + DIGIT_BIT) / DIGIT_BIT);
          /*  Unlikely. */
          if (len < 0) {
             ix--;
