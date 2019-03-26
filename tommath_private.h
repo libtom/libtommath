6d63370370ae03961a933bfa4daf5b995755f4bb
Francois Perrad <francois.perrad@gadz.org>
2019-03-26 18:51:35 +0100

    explicit operator precedence

--- tommath_private.h
+++ tommath_private.h
@@ -90,7 +90,8 @@ extern const size_t mp_s_rmap_reverse_sz;
 int func_name (mp_int * a, type b)                       \
 {                                                        \
    int x = 0;                                            \
-   int res = mp_grow(a, (CHAR_BIT * sizeof(type) + DIGIT_BIT - 1) / DIGIT_BIT); \
+   int new_size = (((CHAR_BIT * sizeof(type)) + DIGIT_BIT) - 1) / DIGIT_BIT; \
+   int res = mp_grow(a, new_size);                       \
    if (res == MP_OKAY) {                                 \
      mp_zero(a);                                         \
      while (b) {                                         \
