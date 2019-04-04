f38e56c7457eb7d4cd074d0a1780e6085f23ccdf
Daniel Mendler <mail@daniel-mendler.de>
2019-04-04 12:57:22 +0200

    remove mp_set_long specialization
    any reasonably intelligent compiler will optimize the loop away

--- bn_mp_set_long.c
+++ bn_mp_set_long.c
@@ -12,25 +12,7 @@
  * SPDX-License-Identifier: Unlicense
  */
 
-/* set a platform dependent unsigned long int */
-#if (ULONG_MAX != 0xFFFFFFFFUL) || (DIGIT_BIT < 32)
 MP_SET_XLONG(mp_set_long, unsigned long)
-#else
-int mp_set_long(mp_int *a, unsigned long b)
-{
-   int x = 0;
-   int res = mp_grow(a, (CHAR_BIT * sizeof(unsigned long) + DIGIT_BIT - 1) / DIGIT_BIT);
-   if (res == MP_OKAY) {
-      mp_zero(a);
-      if (b) {
-         a->dp[x++] = (mp_digit)b;
-      }
-      a->used = x;
-   }
-   return res;
-}
-
-#endif
 #endif
 
 /* ref:         $Format:%D$ */
