27524297700c7c4658ae471d95c41cd62ead97b7
Daniel Mendler <mail@daniel-mendler.de>
2019-04-08 23:23:52 +0200

    apply to bn_mp_sqr

--- bn_mp_sqr.c
+++ bn_mp_sqr.c
@@ -8,35 +8,22 @@ int mp_sqr(const mp_int *a, mp_int *b)
 {
    int     res;
 
-#ifdef BN_MP_TOOM_SQR_C
-   /* use Toom-Cook? */
-   if (a->used >= TOOM_SQR_CUTOFF) {
+   if (MP_ENABLED(MP_TOOM_SQR) && /* use Toom-Cook? */
+       a->used >= TOOM_SQR_CUTOFF) {
       res = mp_toom_sqr(a, b);
-      /* Karatsuba? */
-   } else
-#endif
-#ifdef BN_MP_KARATSUBA_SQR_C
-      if (a->used >= KARATSUBA_SQR_CUTOFF) {
-         res = mp_karatsuba_sqr(a, b);
-      } else
-#endif
-      {
-#ifdef BN_FAST_S_MP_SQR_C
-         /* can we use the fast comba multiplier? */
-         if ((((a->used * 2) + 1) < (int)MP_WARRAY) &&
-             (a->used <
-              (int)(1u << (((CHAR_BIT * sizeof(mp_word)) - (2u * (size_t)DIGIT_BIT)) - 1u)))) {
-            res = fast_s_mp_sqr(a, b);
-         } else
-#endif
-         {
-#ifdef BN_S_MP_SQR_C
-            res = s_mp_sqr(a, b);
-#else
-            res = MP_VAL;
-#endif
-         }
-      }
+   } else if (MP_ENABLED(MP_KARATSUBA_SQR) &&  /* Karatsuba? */
+              a->used >= KARATSUBA_SQR_CUTOFF) {
+      res = mp_karatsuba_sqr(a, b);
+   } else if (MP_ENABLED(FAST_S_MP_SQR) && /* can we use the fast comba multiplier? */
+              (((a->used * 2) + 1) < (int)MP_WARRAY) &&
+              (a->used <
+               (int)(1u << (((CHAR_BIT * sizeof(mp_word)) - (2u * (size_t)DIGIT_BIT)) - 1u)))) {
+      res = fast_s_mp_sqr(a, b);
+   } else if (MP_ENABLED(S_MP_SQR)) {
+      res = s_mp_sqr(a, b);
+   } else {
+      res = MP_VAL;
+   }
    b->sign = MP_ZPOS;
    return res;
 }
