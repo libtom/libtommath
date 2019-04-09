87292ed10a475249038678dbf9a69143aa1f0f5f
Daniel Mendler <mail@daniel-mendler.de>
2019-04-09 12:19:40 +0200

    move mp_balance_mul to tommath_private.h

--- tommath_private.h
+++ tommath_private.h
@@ -50,6 +50,7 @@ int fast_s_mp_mul_high_digs(const mp_int *a, const mp_int *b, mp_int *c, int dig
 int s_mp_mul_high_digs(const mp_int *a, const mp_int *b, mp_int *c, int digs);
 int fast_s_mp_sqr(const mp_int *a, mp_int *b);
 int s_mp_sqr(const mp_int *a, mp_int *b);
+int mp_balance_mul(const mp_int *a, const mp_int *b, mp_int *c);
 int mp_karatsuba_mul(const mp_int *a, const mp_int *b, mp_int *c);
 int mp_toom_mul(const mp_int *a, const mp_int *b, mp_int *c);
 int mp_karatsuba_sqr(const mp_int *a, mp_int *b);
