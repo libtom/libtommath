b0938d70f3ffe77435afcc5e9a7bc9dfb2857991
Daniel Mendler <mail@daniel-mendler.de>
2019-05-24 11:48:27 +0200

    remove obsolete comment

--- tommath.h
+++ tommath.h
@@ -81,9 +81,7 @@ typedef uint64_t             private_mp_word;
  */
 #      define MP_DIGIT_BIT 31
 #   else
-/* default case is 28-bit digits, defines MP_28BIT as a handy macro to test
- * this is to make porting into LibTomCrypt easier :-)
- */
+/* default case is 28-bit digits, defines MP_28BIT as a handy macro to test */
 #      define MP_DIGIT_BIT 28
 #      define MP_28BIT
 #   endif
