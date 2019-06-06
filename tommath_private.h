6c39b07bc24a9ccc6e30a86c990c4ae70db09ab3
Steffen Jaeckel <s@jaeckel.eu>
2019-06-06 11:44:02 +0200

    split up again into separate files

--- tommath_private.h
+++ tommath_private.h
@@ -231,4 +231,53 @@ MP_DEPRECATED(s_mp_toom_mul) mp_err mp_toom_mul(const mp_int *a, const mp_int *b
 MP_DEPRECATED(s_mp_toom_sqr) mp_err mp_toom_sqr(const mp_int *a, mp_int *b);
 MP_DEPRECATED(s_mp_reverse) void bn_reverse(unsigned char *s, int len);
 
+/* code-generating macros */
+#define MP_SET_UNSIGNED(name, w)                                        \
+    void name(mp_int * a, uint##w##_t b)                                \
+    {                                                                   \
+        int i = 0;                                                      \
+        while (b != 0u) {                                               \
+            a->dp[i++] = ((mp_digit)b & MP_MASK);                       \
+            if (w <= MP_DIGIT_BIT) { break; }                           \
+            b >>= ((w <= MP_DIGIT_BIT) ? 0 : MP_DIGIT_BIT);             \
+        }                                                               \
+        a->used = i;                                                    \
+        a->sign = MP_ZPOS;                                              \
+        MP_ZERO_DIGITS(a->dp + a->used, a->alloc - a->used);            \
+    }
+#define MP_SET_SIGNED(name, uname, w)                                   \
+    void name(mp_int * a, int##w##_t b)                                 \
+    {                                                                   \
+        uname(a, (b < 0) ? -(uint##w##_t)b : (uint##w##_t)b);           \
+        if (b < 0) { a->sign = MP_NEG; }                                \
+    }
+#define MP_INIT_INT(name , set, type)                     \
+    mp_err name(mp_int * a, type b)                      \
+    {                                                    \
+        mp_err err;                                      \
+        if ((err = mp_init(a)) != MP_OKAY) {             \
+            return err;                                  \
+        }                                                \
+        set(a, b);                                       \
+        return MP_OKAY;                                  \
+    }
+#define MP_GET_MAG(name, w)                                             \
+    uint##w##_t name(const mp_int* a)                                   \
+    {                                                                   \
+        unsigned i = MP_MIN((unsigned)a->used, (unsigned)((w + MP_DIGIT_BIT - 1) / MP_DIGIT_BIT)); \
+        uint##w##_t res = 0u;                                           \
+        while (i --> 0u) {                                              \
+            res <<= ((w <= MP_DIGIT_BIT) ? 0 : MP_DIGIT_BIT);           \
+            res |= (uint##w##_t)a->dp[i];                               \
+            if (w <= MP_DIGIT_BIT) { break; }                           \
+        }                                                               \
+        return res;                                                     \
+    }
+#define MP_GET_SIGNED(name, mag, w)                                     \
+    int##w##_t name(const mp_int* a)                                    \
+    {                                                                   \
+        uint64_t res = mag(a);                                          \
+        return (a->sign == MP_NEG) ? (int##w##_t)-res : (int##w##_t)res;\
+    }
+
 #endif
