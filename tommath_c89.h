8ecf7eaea46b4f895222546eb5678a7201d5654c
Daniel Mendler <mail@daniel-mendler.de>
2019-10-27 09:19:32 +0100

    better comment in tommath_c89.h

--- tommath_c89.h
+++ tommath_c89.h
@@ -1,5 +1,23 @@
-/* please adapt this header to your needs */
+/* LibTomMath, multiple-precision integer library -- Tom St Denis */
+/* SPDX-License-Identifier: Unlicense */
+
+/*
+ * This header defines custom types which
+ * are used in c89 mode.
+ *
+ * By default, the source uses stdbool.h
+ * and stdint.h. The command `make c89`
+ * can be used to convert the source,
+ * such that this header is used instead.
+ * Use `make c99` to convert back.
+ *
+ * Please adapt the following definitions to your needs!
+ */
+
+/* stdbool.h replacement types */
 typedef enum { MP_NO, MP_YES } mp_bool;
+
+/* stdint.h replacement types */
 typedef __INT8_TYPE__   mp_i8;
 typedef __INT16_TYPE__  mp_i16;
 typedef __INT32_TYPE__  mp_i32;
@@ -8,6 +26,8 @@ typedef __UINT8_TYPE__  mp_u8;
 typedef __UINT16_TYPE__ mp_u16;
 typedef __UINT32_TYPE__ mp_u32;
 typedef __UINT64_TYPE__ mp_u64;
+
+/* inttypes.h replacement, printf format specifier */
 # if __WORDSIZE == 64
 #  define MP_PRI64_PREFIX "l"
 # else
