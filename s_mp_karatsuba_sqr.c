39e077a9154082beaca1e4a5a43dbe4f54505823
Daniel Mendler <mail@daniel-mendler.de>
2019-10-29 20:05:30 +0100

    simplifications: toom and karatsuba

--- s_mp_karatsuba_sqr.c
+++ s_mp_karatsuba_sqr.c
@@ -13,8 +13,8 @@
 mp_err s_mp_karatsuba_sqr(const mp_int *a, mp_int *b)
 {
    mp_int  x0, x1, t1, t2, x0x0, x1x1;
-   int     B;
-   mp_err  err = MP_MEM;
+   int B, x;
+   mp_err  err;
 
    /* min # of digits */
    B = a->used;
@@ -23,37 +23,27 @@ mp_err s_mp_karatsuba_sqr(const mp_int *a, mp_int *b)
    B = B >> 1;
 
    /* init copy all the temps */
-   if (mp_init_size(&x0, B) != MP_OKAY)
+   if ((err = mp_init_size(&x0, B)) != MP_OKAY)
       goto LBL_ERR;
-   if (mp_init_size(&x1, a->used - B) != MP_OKAY)
+   if ((err = mp_init_size(&x1, a->used - B)) != MP_OKAY)
       goto X0;
 
    /* init temps */
-   if (mp_init_size(&t1, a->used * 2) != MP_OKAY)
+   if ((err = mp_init_size(&t1, a->used * 2)) != MP_OKAY)
       goto X1;
-   if (mp_init_size(&t2, a->used * 2) != MP_OKAY)
+   if ((err = mp_init_size(&t2, a->used * 2)) != MP_OKAY)
       goto T1;
-   if (mp_init_size(&x0x0, B * 2) != MP_OKAY)
+   if ((err = mp_init_size(&x0x0, B * 2)) != MP_OKAY)
       goto T2;
-   if (mp_init_size(&x1x1, (a->used - B) * 2) != MP_OKAY)
+   if ((err = mp_init_size(&x1x1, (a->used - B) * 2)) != MP_OKAY)
       goto X0X0;
 
-   {
-      int x;
-      mp_digit *dst, *src;
-
-      src = a->dp;
-
-      /* now shift the digits */
-      dst = x0.dp;
-      for (x = 0; x < B; x++) {
-         *dst++ = *src++;
-      }
-
-      dst = x1.dp;
-      for (x = B; x < a->used; x++) {
-         *dst++ = *src++;
-      }
+   /* now shift the digits */
+   for (x = 0; x < B; x++) {
+      x0.dp[x] = a->dp[x];
+   }
+   for (x = B; x < a->used; x++) {
+      x1.dp[x - B] = a->dp[x];
    }
 
    x0.used = B;
@@ -62,35 +52,33 @@ mp_err s_mp_karatsuba_sqr(const mp_int *a, mp_int *b)
    mp_clamp(&x0);
 
    /* now calc the products x0*x0 and x1*x1 */
-   if (mp_sqr(&x0, &x0x0) != MP_OKAY)
+   if ((err = mp_sqr(&x0, &x0x0)) != MP_OKAY)
       goto X1X1;           /* x0x0 = x0*x0 */
-   if (mp_sqr(&x1, &x1x1) != MP_OKAY)
+   if ((err = mp_sqr(&x1, &x1x1)) != MP_OKAY)
       goto X1X1;           /* x1x1 = x1*x1 */
 
    /* now calc (x1+x0)**2 */
-   if (s_mp_add(&x1, &x0, &t1) != MP_OKAY)
+   if ((err = s_mp_add(&x1, &x0, &t1)) != MP_OKAY)
       goto X1X1;           /* t1 = x1 - x0 */
-   if (mp_sqr(&t1, &t1) != MP_OKAY)
+   if ((err = mp_sqr(&t1, &t1)) != MP_OKAY)
       goto X1X1;           /* t1 = (x1 - x0) * (x1 - x0) */
 
    /* add x0y0 */
-   if (s_mp_add(&x0x0, &x1x1, &t2) != MP_OKAY)
+   if ((err = s_mp_add(&x0x0, &x1x1, &t2)) != MP_OKAY)
       goto X1X1;           /* t2 = x0x0 + x1x1 */
-   if (s_mp_sub(&t1, &t2, &t1) != MP_OKAY)
+   if ((err = s_mp_sub(&t1, &t2, &t1)) != MP_OKAY)
       goto X1X1;           /* t1 = (x1+x0)**2 - (x0x0 + x1x1) */
 
    /* shift by B */
-   if (mp_lshd(&t1, B) != MP_OKAY)
+   if ((err = mp_lshd(&t1, B)) != MP_OKAY)
       goto X1X1;           /* t1 = (x0x0 + x1x1 - (x1-x0)*(x1-x0))<<B */
-   if (mp_lshd(&x1x1, B * 2) != MP_OKAY)
+   if ((err = mp_lshd(&x1x1, B * 2)) != MP_OKAY)
       goto X1X1;           /* x1x1 = x1x1 << 2*B */
 
-   if (mp_add(&x0x0, &t1, &t1) != MP_OKAY)
+   if ((err = mp_add(&x0x0, &t1, &t1)) != MP_OKAY)
       goto X1X1;           /* t1 = x0x0 + t1 */
-   if (mp_add(&t1, &x1x1, b) != MP_OKAY)
+   if ((err = mp_add(&t1, &x1x1, b)) != MP_OKAY)
       goto X1X1;           /* t1 = x0x0 + t1 + x1x1 */
-
-   err = MP_OKAY;
 
 X1X1:
    mp_clear(&x1x1);
