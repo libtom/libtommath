f6ae8420ddee3a1a0bf5f8939e2a605f30406655
Daniel Mendler <mail@daniel-mendler.de>
2019-10-29 20:08:42 +0100

    simplifications: reduce functions

--- s_mp_montgomery_reduce_fast.c
+++ s_mp_montgomery_reduce_fast.c
@@ -13,7 +13,7 @@
 */
 mp_err s_mp_montgomery_reduce_fast(mp_int *x, const mp_int *n, mp_digit rho)
 {
-   int     ix, olduse;
+   int     ix, oldused;
    mp_err  err;
    mp_word W[MP_WARRAY];
 
@@ -22,7 +22,7 @@ mp_err s_mp_montgomery_reduce_fast(mp_int *x, const mp_int *n, mp_digit rho)
    }
 
    /* get old used count */
-   olduse = x->used;
+   oldused = x->used;
 
    /* grow a as required */
    if (x->alloc < (n->used + 1)) {
@@ -34,38 +34,30 @@ mp_err s_mp_montgomery_reduce_fast(mp_int *x, const mp_int *n, mp_digit rho)
    /* first we have to get the digits of the input into
     * an array of double precision words W[...]
     */
-   {
-      mp_word *_W;
-      mp_digit *tmpx;
 
-      /* alias for the W[] array */
-      _W   = W;
+   /* copy the digits of a into W[0..a->used-1] */
+   for (ix = 0; ix < x->used; ix++) {
+      W[ix] = x->dp[ix];
+   }
 
-      /* alias for the digits of  x*/
-      tmpx = x->dp;
-
-      /* copy the digits of a into W[0..a->used-1] */
-      for (ix = 0; ix < x->used; ix++) {
-         *_W++ = *tmpx++;
-      }
-
-      /* zero the high words of W[a->used..m->used*2] */
-      if (ix < ((n->used * 2) + 1)) {
-         MP_ZERO_BUFFER(_W, sizeof(mp_word) * (size_t)(((n->used * 2) + 1) - ix));
-      }
+   /* zero the high words of W[a->used..m->used*2] */
+   if (ix < ((n->used * 2) + 1)) {
+      MP_ZERO_BUFFER(W + x->used, sizeof(mp_word) * (size_t)(((n->used * 2) + 1) - ix));
    }
 
    /* now we proceed to zero successive digits
     * from the least significant upwards
     */
    for (ix = 0; ix < n->used; ix++) {
+      int iy;
+      mp_digit mu;
+
       /* mu = ai * m' mod b
        *
        * We avoid a double precision multiplication (which isn't required)
        * by casting the value down to a mp_digit.  Note this requires
        * that W[ix-1] have  the carry cleared (see after the inner loop)
        */
-      mp_digit mu;
       mu = ((W[ix] & MP_MASK) * rho) & MP_MASK;
 
       /* a = a + mu * m * b**i
@@ -82,21 +74,8 @@ mp_err s_mp_montgomery_reduce_fast(mp_int *x, const mp_int *n, mp_digit rho)
        * carry fixups are done in order so after these loops the
        * first m->used words of W[] have the carries fixed
        */
-      {
-         int iy;
-         mp_digit *tmpn;
-         mp_word *_W;
-
-         /* alias for the digits of the modulus */
-         tmpn = n->dp;
-
-         /* Alias for the columns set by an offset of ix */
-         _W = W + ix;
-
-         /* inner loop */
-         for (iy = 0; iy < n->used; iy++) {
-            *_W++ += (mp_word)mu * (mp_word)*tmpn++;
-         }
+      for (iy = 0; iy < n->used; iy++) {
+         W[ix + iy] += (mp_word)mu * (mp_word)n->dp[iy];
       }
 
       /* now fix carry for next digit, W[ix+1] */
@@ -107,47 +86,30 @@ mp_err s_mp_montgomery_reduce_fast(mp_int *x, const mp_int *n, mp_digit rho)
     * shift the words downward [all those least
     * significant digits we zeroed].
     */
-   {
-      mp_digit *tmpx;
-      mp_word *_W, *_W1;
 
-      /* nox fix rest of carries */
-
-      /* alias for current word */
-      _W1 = W + ix;
-
-      /* alias for next word, where the carry goes */
-      _W = W + ++ix;
-
-      for (; ix < ((n->used * 2) + 1); ix++) {
-         *_W++ += *_W1++ >> (mp_word)MP_DIGIT_BIT;
-      }
-
-      /* copy out, A = A/b**n
-       *
-       * The result is A/b**n but instead of converting from an
-       * array of mp_word to mp_digit than calling mp_rshd
-       * we just copy them in the right order
-       */
-
-      /* alias for destination word */
-      tmpx = x->dp;
-
-      /* alias for shifted double precision result */
-      _W = W + n->used;
-
-      for (ix = 0; ix < (n->used + 1); ix++) {
-         *tmpx++ = *_W++ & (mp_word)MP_MASK;
-      }
-
-      /* zero oldused digits, if the input a was larger than
-       * m->used+1 we'll have to clear the digits
-       */
-      MP_ZERO_DIGITS(tmpx, olduse - ix);
+   for (; ix < (n->used * 2); ix++) {
+      W[ix + 1] += W[ix] >> (mp_word)MP_DIGIT_BIT;
    }
 
-   /* set the max used and clamp */
+   /* copy out, A = A/b**n
+    *
+    * The result is A/b**n but instead of converting from an
+    * array of mp_word to mp_digit than calling mp_rshd
+    * we just copy them in the right order
+    */
+
+   for (ix = 0; ix < (n->used + 1); ix++) {
+      x->dp[ix] = W[n->used + ix] & (mp_word)MP_MASK;
+   }
+
+   /* set the max used */
    x->used = n->used + 1;
+
+   /* zero oldused digits, if the input a was larger than
+    * m->used+1 we'll have to clear the digits
+    */
+   MP_ZERO_DIGITS(x->dp + x->used, oldused - x->used);
+
    mp_clamp(x);
 
    /* if A >= m then A = A - m */
