dedbd25e5f4ca9e6978f617702a757721acbd9ce
czurnieden <czurnieden@gmx.de>
2019-11-09 00:00:07 +0100

    reinstated resetting and correctied printout in case of cut-offs given at command-line

--- etc/tune.c
+++ etc/tune.c
@@ -467,8 +467,12 @@ int main(int argc, char **argv)
          if (test[n].fn != NULL) {
             s_run(test[n].name, test[n].fn, test[n].cutoff);
             *test[n].update = *test[n].cutoff;
+            *test[n].cutoff = INT_MAX;
          }
       }
+   }
+   if (printpreset == 1) {
+      updated = orig;
    }
    if (args.terse == 1) {
       printf("%d %d %d %d\n",
