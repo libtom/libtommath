ae0c9cb208e9ab576d7d0a6689ccf0cf39a06253
spaette <lindblad@gmx.us>
2022-10-02 12:53:57 +0200

    Add typos.sh script

---
+++ typos.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+sed -i "s/Dimminished/Diminished/g" libtommath/logs/before_after.dem
+sed -i "s/Dimminished/Diminished/g" libtommath/logs/graphs.dem
+sed -i "s/accomodate/accommodate/g" libtommath/mp_mul_2.c
+sed -i "s/accomodate/accommodate/g" libtommath/mp_2expt.c
+sed -i "s/addtional/additional/g" libtommath/mp_prime_is_prime.c
+sed -i "s/alot/a lot/g" libtommath/changes.txt
+sed -i "s/alot/a lot/g" libtommath/mp_exptmod.c
+sed -i "s/alot/a lot/g" libtommath/mp_montgomery_calc_normalization.c
+sed -i "s/annonying/annoying/g" libtommath/astylerc
+sed -i "s/asymptopically/asymptotically/g" libtommath/s_mp_mul_karatsuba.c
+sed -i "s/calulates/calculates/g" libtommath/mp_reduce_setup.c
+sed -i "s/configuraion/configuration/g" libtommath/doc/makefile
+sed -i "s/eliminiation/elimination/g" libtommath/CMakeLists.txt
+sed -i "s/embeded/embedded/g" libtommath/mp_prime_is_prime.c
+sed -i "s/endianess/endianness/g" libtommath/demo/test.c
+sed -i "s/iterrate/iterate/g" libtommath/s_mp_sqr_comba.c
+sed -i "s/iterrate/iterate/g" libtommath/s_mp_mul_high_comba.c
+sed -i "s/iterrate/iterate/g" libtommath/s_mp_mul_comba.c
+sed -i "s/maginitude/magnitude/g" libtommath/mp_cmp_mag.c
+sed -i "s/offseting/offsetting/g" libtommath/s_mp_montgomery_reduce_comba.c
+sed -i "s/ofthe/of the/g" libtommath/doc/bn.tex
+sed -i "s/otherway/other way/g" libtommath/mp_lshd.c
+sed -i "s/packagage/package/g" libtommath/changes.txt
+sed -i "s/preemptivly/preemptively/g" libtommath/mp_prime_is_prime.c
+sed -i "s/probabilty/probability/g" libtommath/doc/bn.tex
+sed -i "s/programms/programs/g" libtommath/doc/bn.tex
+sed -i "s/seperate/separate/g" libtommath/changes.txt
+sed -i "s/simplifiy/simplify/g" libtommath/demo/test.c
+sed -i "s/tollchocked/tolchocked/g" libtommath/mtest/mpi.c
+sed -i "s/trimed/trimmed/g" libtommath/mp_clamp.c
+sed -i "s/triming/trimming/g" libtommath/demo/mtest_opponent.c
+sed -i "s/uncommited/uncommitted/g" libtommath/makefile
+sed -i "s/unsused/unused/g" libtommath/changes.txt
