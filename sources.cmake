fdcc1be7b824bb307947df7a9ae40aab5ba1f290
Steffen Jaeckel <s@jaeckel.eu>
2023-05-09 10:22:06 +0200

    Update makefiles
    Signed-off-by: Steffen Jaeckel <s@jaeckel.eu>

--- sources.cmake
+++ sources.cmake
@@ -122,6 +122,8 @@ mp_to_sbin.c
 mp_to_ubin.c
 mp_ubin_size.c
 mp_unpack.c
+mp_warray_free.c
+mp_warray_init.c
 mp_xor.c
 mp_zero.c
 s_mp_add.c
@@ -156,6 +158,10 @@ s_mp_sqr_comba.c
 s_mp_sqr_karatsuba.c
 s_mp_sqr_toom.c
 s_mp_sub.c
+s_mp_warray.c
+s_mp_warray_free.c
+s_mp_warray_get.c
+s_mp_warray_put.c
 s_mp_zero_buf.c
 s_mp_zero_digs.c
 )
